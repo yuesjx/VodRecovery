ca7cd2f3071c1f9e1e2ebbf1a7027d1e969c3a02
macielg1 <guflumaciel@gmail.com>
2024-02-16 19:26:29 -0300

    updated install dependencies

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,7 +1,25 @@
+
+
 import os
+import importlib
+
+def check_package(package_name):
+    try:
+        importlib.import_module(package_name)
+        return True
+    except ImportError:
+        return False
 
 def install_requirements():
-    os.system('pip install -r requirements.txt')
+    requirements_file = 'requirements.txt'
+    with open(requirements_file) as f:
+        packages = f.read().splitlines()
+    for package in packages:
+        if not check_package(package):
+            print(f"Installing {package}...")
+            os.system(f'pip install {package}')
+        else:
+            print(f"{package} is already installed.")
 
 if __name__ == '__main__':
     install_requirements()
