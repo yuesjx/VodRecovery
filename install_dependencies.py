a0562325f577d9e97f5f8b2f99d4c7f4e7e7999d
MacielG1 <guflumaciel@gmail.com>
2024-09-26 12:07:19 -0300

    better error handling

--- install_dependencies.py
+++ install_dependencies.py
@@ -12,12 +12,12 @@ def install_requirements(requirements_path):
         if not check_package(package):
             try:
                 print(f"Installing {package}...")
-                subprocess.run([sys.executable, "-m", "pip", "install", package, "-q"], check=True)
+                subprocess.run([sys.executable, "-m", "pip", "install", package, "-q", "--no-warn-conflicts"], check=True)
             except subprocess.CalledProcessError:
                 # Install latest version
                 print(f"\n\033[34mFailed to install {package}. Trying again...\033[0m")
                 print(f"Installing {package.split('==')[0]}...")
-                subprocess.run([sys.executable, "-m", "pip", "install", package.split('==')[0], "-q"], check=True)
+                subprocess.run([sys.executable, "-m", "pip", "install", package.split('==')[0], "-q", "--no-warn-conflicts"], check=True)
 
 
 def check_package(package_name):
@@ -30,9 +30,10 @@ def check_package(package_name):
 
 def update_pip():
     try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip", "-q"])
-    except subprocess.CalledProcessError as err:
-        print(f"Error updating pip: {err}")
+        subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pip", "-q"],
+                       capture_output=True, check=False)
+    except Exception as e:
+        print(f"Error updating pip: {str(e)}")
 
 
 if __name__ == "__main__":
@@ -47,10 +48,10 @@ if __name__ == "__main__":
         vod_recovery_script = os.path.join(script_dir, 'vod_recovery.py')
 
         install_requirements(requirements_file)
-        os.system(f'python "{install_ffmpeg_script}"')
-        os.system(f'python "{vod_recovery_script}"')
+        subprocess.run([sys.executable, install_ffmpeg_script], check=False)
+        subprocess.run([sys.executable, vod_recovery_script], check=False)
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred: {str(e)}")
         input("\nPress Enter to continue...")
 
